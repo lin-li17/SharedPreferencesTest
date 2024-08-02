f477d86e4c7d2553b08606f9c51176bf2441dc61
淋漓 <2564576814@qq.com>
2024-08-02 15:15:28 +0800

    读取

--- app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.sharedpreferencestest;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age",20);
                 editor.putBoolean("married",false);
                 editor.apply();
+            }
+        });
+        Button button1 =findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age = pref.getInt("age",0);
+                boolean married = pref.getBoolean("married",false);
+                TextView tv = findViewById(R.id.textView);
+                tv.setText("姓名:"+ name + "年龄:" + age + "婚配:" + married);
             }
         });
     }
